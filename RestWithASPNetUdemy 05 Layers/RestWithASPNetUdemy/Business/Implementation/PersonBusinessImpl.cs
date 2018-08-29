b546b9e0f10cec100206fae727553c3222779cdd
Eddersouza <eddye_souza@yahoo.com.br>
2018-08-29 12:36:26 -0300

    Layer project

---
+++ RestWithASPNetUdemy 05 Layers/RestWithASPNetUdemy/Business/Implementation/PersonBusinessImpl.cs
@@ -0,0 +1,44 @@
+﻿using RestWithASPNetUdemy.Model;
+using RestWithASPNetUdemy.Model.Context;
+using RestWithASPNetUdemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNetUdemy.Business.Implementation
+{
+    public class PersonBusinessImpl : IPersonBusiness
+    {
+        private IPersonRepository _personRepository;
+
+        public PersonBusinessImpl(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        public Person Create(Person person)
+        {            
+            return _personRepository.Create(person);
+        }
+
+        public void Delete(long id)
+        {
+            _personRepository.Delete(id);
+        }
+
+        public List<Person> FindAll()
+        {
+            return _personRepository.FindAll();
+        }
+
+        public Person FindById(long id)
+        {
+            return _personRepository.FindById(id);
+        }
+
+        public Person Update(Person person)
+        {
+            return _personRepository.Update(person);
+        }
+    }
+}
---
+++ RestWithASPNetUdemy 05 Layers/RestWithASPNetUdemy/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RestWithASPNetUdemy.Model.Context;
+using RestWithASPNetUdemy.Business;
+using RestWithASPNetUdemy.Business.Implementation;
+using RestWithASPNetUdemy.Repository.Implementation;
+using RestWithASPNetUdemy.Repository;
+
+namespace RestWithASPNetUdemy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["MysqlConnection:MysqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options=>options.UseMySql(connection));
+
+            services.AddMvc();
+
+            services.AddApiVersioning();
+
+            services.AddScoped<IPersonBusiness, PersonBusinessImpl>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImpl>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}

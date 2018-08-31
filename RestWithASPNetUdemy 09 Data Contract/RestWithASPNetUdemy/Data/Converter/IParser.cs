5a05573ece3bb44b02c3d7add65aaa3e18a97df5
Eddersouza <eddye_souza@yahoo.com.br>
2018-08-31 14:45:36 -0300

    Implementing data contract

---
+++ RestWithASPNetUdemy 09 Data Contract/RestWithASPNetUdemy/Data/Converter/IParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNetUdemy.Data.Converter
+{
+    public interface IParser<Origin, Destine>
+    {
+        Destine Parse(Origin origin);
+
+        List<Destine> Parse(List<Origin> origin);
+    }
+}
---
+++ RestWithASPNetUdemy 09 Data Contract/RestWithASPNetUdemy/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithASPNetUdemy.Model.Base;
+using RestWithASPNetUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNetUdemy.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private MySQLContext _context;
+        private DbSet<T> dataSet;
+
+        public GenericRepository(MySQLContext mySQLContext)
+        {
+            _context = mySQLContext;
+            dataSet = _context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            try
+            {
+                dataSet.Add(item);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return item;
+        }
+
+        public void Delete(long id)
+        {
+            var result = dataSet.SingleOrDefault(p => p.Id.Equals(id));
+
+            try
+            {
+                if (result != null) dataSet.Remove(result);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Exists(long id)
+        {
+            return dataSet.Any(p => p.Id.Equals(id));
+        }
+
+        public List<T> FindAll()
+        {
+            return dataSet.ToList();
+        }
+
+        public T FindById(long id)
+        {
+            return dataSet.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public T Update(T item)
+        {
+            if (!Exists(item.Id)) return null;
+
+            var result = dataSet.SingleOrDefault(p => p.Id.Equals(item.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            return result;
+        }
+    }
+}
